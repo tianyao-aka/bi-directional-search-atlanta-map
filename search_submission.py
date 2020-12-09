35acfe0ef044e1797c1fdbcd4fd333d8ffb97ecc
tianyao-aka <75415813+tianyao-aka@users.noreply.github.com>
2020-12-09 14:33:53 +0800

    a project from CS6601-Artificial Intelligence
    implement and optimze bi-diretional search for shortest route finding in Atlanta Map

---
+++ search_submission.py
@@ -0,0 +1,353 @@
+# coding=utf-8
+"""
+This file is your main submission that will be graded against. Only copy-paste
+code on the relevant classes included here. Do not add any classes or functions
+to this file that are not part of the classes that we want.
+"""
+
+from __future__ import division
+
+import heapq
+import os
+import pickle
+
+
+class PriorityQueue(object):
+    """
+    A queue structure where each element is served in order of priority.
+
+    Elements in the queue are popped based on the priority with higher priority
+    elements being served before lower priority elements.  If two elements have
+    the same priority, they will be served in the order they were added to the
+    queue.
+
+    Traditionally priority queues are implemented with heaps, but there are any
+    number of implementation options.
+
+    (Hint: take a look at the module heapq)
+
+    Attributes:
+        queue (list): Nodes added to the priority queue.
+        current (int): The index of the current node in the queue.
+    """
+
+    def __init__(self):
+        """Initialize a new Priority Queue."""
+
+        self.queue = []
+
+    def pop(self):
+        """
+        Pop top priority node from queue.
+
+        Returns:
+            The node with the highest priority.
+        """
+
+        # TODO: finish this function!
+        raise NotImplementedError
+
+    def remove(self, node_id):
+        """
+        Remove a node from the queue.
+
+        This is a hint, you might require this in ucs,
+        however, if you choose not to use it, you are free to
+        define your own method and not use it.
+
+        Args:
+            node_id (int): Index of node in queue.
+        """
+
+        raise NotImplementedError
+
+    def __iter__(self):
+        """Queue iterator."""
+
+        return iter(sorted(self.queue))
+
+    def __str__(self):
+        """Priority Queue to string."""
+
+        return 'PQ:%s' % self.queue
+
+    def append(self, node):
+        """
+        Append a node to the queue.
+
+        Args:
+            node: Comparable Object to be added to the priority queue.
+        """
+
+        # TODO: finish this function!
+        raise NotImplementedError
+
+    def __contains__(self, key):
+        """
+        Containment Check operator for 'in'
+
+        Args:
+            key: The key to check for in the queue.
+
+        Returns:
+            True if key is found in queue, False otherwise.
+        """
+
+        return key in [n for _, n in self.queue]
+
+    def __eq__(self, other):
+        """
+        Compare this Priority Queue with another Priority Queue.
+
+        Args:
+            other (PriorityQueue): Priority Queue to compare against.
+
+        Returns:
+            True if the two priority queues are equivalent.
+        """
+
+        return self == other
+
+    def size(self):
+        """
+        Get the current size of the queue.
+
+        Returns:
+            Integer of number of items in queue.
+        """
+
+        return len(self.queue)
+
+    def clear(self):
+        """Reset queue to empty (no nodes)."""
+
+        self.queue = []
+
+    def top(self):
+        """
+        Get the top item in the queue.
+
+        Returns:
+            The first item stored in teh queue.
+        """
+
+        return self.queue[0]
+
+
+def breadth_first_search(graph, start, goal):
+    """
+    Warm-up exercise: Implement breadth-first-search.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def uniform_cost_search(graph, start, goal):
+    """
+    Warm-up exercise: Implement uniform_cost_search.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def null_heuristic(graph, v, goal):
+    """
+    Null heuristic used as a base line.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        v (str): Key for the node to calculate from.
+        goal (str): Key for the end node to calculate to.
+
+    Returns:
+        0
+    """
+
+    return 0
+
+
+def euclidean_dist_heuristic(graph, v, goal):
+    """
+    Warm-up exercise: Implement the euclidean distance heuristic.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        v (str): Key for the node to calculate from.
+        goal (str): Key for the end node to calculate to.
+
+    Returns:
+        Euclidean distance between `v` node and `goal` node
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def a_star(graph, start, goal, heuristic=euclidean_dist_heuristic):
+    """
+    Warm-up exercise: Implement A* algorithm.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+        heuristic: Function to determine distance heuristic.
+            Default: euclidean_dist_heuristic.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def bidirectional_ucs(graph, start, goal):
+    """
+    Exercise 1: Bidirectional Search.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def bidirectional_a_star(graph, start, goal,
+                         heuristic=euclidean_dist_heuristic):
+    """
+    Exercise 2: Bidirectional A*.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+        heuristic: Function to determine distance heuristic.
+            Default: euclidean_dist_heuristic.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def tridirectional_search(graph, goals):
+    """
+    Exercise 3: Tridirectional UCS Search
+
+    See README.MD for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        goals (list): Key values for the 3 goals
+
+    Returns:
+        The best path as a list from one of the goal nodes (including both of
+        the other goal nodes).
+    """
+    # TODO: finish this function
+    raise NotImplementedError
+
+
+def tridirectional_upgraded(graph, goals, heuristic=euclidean_dist_heuristic):
+    """
+    Exercise 3: Upgraded Tridirectional Search
+
+    See README.MD for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        goals (list): Key values for the 3 goals
+        heuristic: Function to determine distance heuristic.
+            Default: euclidean_dist_heuristic.
+
+    Returns:
+        The best path as a list from one of the goal nodes (including both of
+        the other goal nodes).
+    """
+    # TODO: finish this function
+    raise NotImplementedError
+
+
+def return_your_name():
+    """Return your name from this function"""
+    # TODO: finish this function
+    raise NotImplementedError
+
+
+# Extra Credit: Your best search method for the race
+def custom_search(graph, start, goal, data=None):
+    """
+    Race!: Implement your best search algorithm here to compete against the
+    other student agents.
+
+    See README.md for exercise description.
+
+    Args:
+        graph (ExplorableGraph): Undirected graph to search.
+        start (str): Key for the start node.
+        goal (str): Key for the end node.
+        data :  Data used in the custom search.
+            Default: None.
+
+    Returns:
+        The best path as a list from the start and goal nodes (including both).
+    """
+
+    # TODO: finish this function!
+    raise NotImplementedError
+
+
+def load_data():
+    """
+    Loads data from data.pickle and return the data object that is passed to
+    the custom_search method.
+
+    Will be called only once. Feel free to modify.
+
+    Returns:
+         The data loaded from the pickle file.
+    """
+
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+    pickle_file_path = os.path.join(dir_name, "data.pickle")
+    data = pickle.load(open(pickle_file_path, 'rb'))
+    return data
